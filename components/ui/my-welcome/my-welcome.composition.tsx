b512a47512c6fa0520cdf52321743654bbdb8463
Brian M <brian@limelighthealth.com>
2022-04-08 11:28:47 -0400

    latest pull

--- components/ui/my-welcome/my-welcome.composition.tsx
+++ components/ui/my-welcome/my-welcome.composition.tsx
@@ -3,6 +3,6 @@ import { MyWelcome } from './my-welcome';
 
 export const BasicMyWelcome = () => {
   return (
-    <MyWelcome>hello world!!!!!!!</MyWelcome>
+    <MyWelcome>hello world!!!!!!!!</MyWelcome>
   );
 }
